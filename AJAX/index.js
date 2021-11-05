94d2295ff5a9e4715126fac9bea4e28f5c2927e2
Albert <albert.barsamov@gmail.com>
2021-11-05 23:08:22 +0100

    first request - ok

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,1 +1,15 @@
+const resultBlock = document.querySelector("#result");
+const clickMeButton = document.querySelector("#clickMe");
 
+clickMeButton.addEventListener("click", makeRequest);
+
+function makeRequest() {
+  resultBlock.innerHTML = "hello world";
+}
+
+var a = 5;
+
+$.ajax("https://repetitora.net/api/js/Images");
+
+a = 6;
+console.log(a);
