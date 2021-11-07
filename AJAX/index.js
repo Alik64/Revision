80ece38488bbf2c6173f29d67a3b7a05c6379fa6
Albert <albert.barsamov@gmail.com>
2021-11-07 23:47:26 +0100

    hw

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,6 +1,10 @@
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#clickMe");
 const pageNumber = document.querySelector("#page-number");
+// Home work
+const ajoutImage = document.querySelector("#ajoutImage");
+const divHomeWork = document.querySelector("#homeWork");
+const pageNumberHW = document.querySelector("#page-number-left");
 
 function onDataReceived(data) {
   data.forEach((el) => {
@@ -9,6 +13,18 @@ function onDataReceived(data) {
     resultBlock.appendChild(img);
   });
 }
+
+function ajoutImages(data) {
+  data.forEach((image) => {
+    const img = document.createElement("img");
+    img.src = image.thumbnail;
+    divHomeWork.appendChild(img);
+  });
+}
+
+ajoutImage.addEventListener("click", () => {
+  getImages(pageNumberHW.value, ajoutImages);
+});
 
 clickMeButton.addEventListener("click", () => {
   getImages(pageNumber.value, onDataReceived);
