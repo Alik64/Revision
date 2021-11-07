274642445af4bb46e8678fdb84ba662e53fcd8fd
Albert <albert.barsamov@gmail.com>
2021-11-07 22:17:47 +0100

    ajout boutton de'envoie

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,23 +1,17 @@
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#clickMe");
+const pageNum = document.querySelector("#page-number");
 
 clickMeButton.addEventListener("click", makeRequest);
 
 function makeRequest() {
-  resultBlock.innerHTML = "hello world";
+  $.ajax(`https://repetitora.net/api/js/Images?page=${pageNum.value}&count=1`, {
+    success: (data) => {
+      data.forEach((el) => {
+        const img = document.createElement("img");
+        img.src = el.thumbnail;
+        resultBlock.appendChild(img);
+      });
+    },
+  });
 }
-
-var a = 5;
-
-$.ajax("https://repetitora.net/api/js/Images", {
-  success: (data) => {
-    data.forEach((el) => {
-      const img = document.createElement("img");
-      img.src = el.thumbnail;
-      document.body.appendChild(img);
-    });
-  },
-});
-
-a = 6;
-console.log(a);
