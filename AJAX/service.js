c06c3ac593e45230fc8cc29aa4cb191d2014c0eb
Albert <albert.barsamov@gmail.com>
2021-11-11 13:52:44 +0100

    maj

--- AJAX/service.js
+++ AJAX/service.js
@@ -6,6 +6,7 @@
 //   return promise;
 // }
 
+// GET
 function getImages(pageNumber) {
   const promise = axios.get(
     `https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`
@@ -18,18 +19,40 @@ function getImages(pageNumber) {
 
 function getTasks(taskName) {
   const promise = axios.get(
-    `https://repetitora.net/api/JS/Tasks?widgetId=090909&page=30`
+    `https://repetitora.net/api/JS/Tasks?widgetId=090909&count=30`
   );
   return promise.then((response) => {
     return response.data;
   });
 }
+// POST
 
 function createTask(title) {
   const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
     widgetId: 090909,
     title: title,
   });
+  return promise.then((response) => {
+    return response.data;
+  });
+}
+
+// PUT
+function updateTask(title) {
+  const promise = axios.put(`https://repetitora.net/api/JS/Tasks`, {
+    widgetId: 090909,
+    title: title,
+  });
+  return promise.then((response) => {
+    return response.data;
+  });
+}
+
+// DELETE
+function deleteTask(id) {
+  const promise = axios.delete(
+    `https://repetitora.net/api/JS/Tasks?widgetId=090909&taskId=${id}`
+  );
   return promise.then((response) => {
     return response.data;
   });
