5dbd560b0150387f2c4d5dbf503eb8cc4bac48d9
Albert <albert.barsamov@gmail.com>
2021-11-13 00:18:25 +0100

    render

--- Divers/creationObjets.js
+++ Divers/creationObjets.js
@@ -1,13 +1,25 @@
+// Class components
+
 // 2 способы создания обЪектов
 
 // 1 - с помощью Класса
-class Man {
-  constructor(name, age) {
-    (this.name = name), (this.age = age);
-  }
-}
+
 let m1 = new Man("John", 24);
 
 // 2 - Литерал объекта
 
 let m_1 = { name: "Billy", age: 35 };
+
+class Man {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+  render() {
+    return (
+      <div>
+        {this.name}
+        {this.age}
+      </div>
+    );
+  }
+}
