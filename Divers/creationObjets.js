b2c9a6ea7e177748543b9545a54498acbe50da17
Albert <albert.barsamov@gmail.com>
2021-11-13 00:29:22 +0100

    class component update

--- Divers/creationObjets.js
+++ Divers/creationObjets.js
@@ -3,12 +3,7 @@
 // 2 способы создания обЪектов
 
 // 1 - с помощью Класса
-
-let m1 = new Man("John", 24);
-
-// 2 - Литерал объекта
-
-let m_1 = { name: "Billy", age: 35 };
+// 2 - Литерал объекта => let m_1 = { name: "Billy", age: 35 };
 
 class Man {
   constructor(name, age) {
@@ -23,3 +18,28 @@ class Man {
     );
   }
 }
+
+let m2 = new Man("Bob", 56);
+
+// Классовая компонента
+class ManC {
+  constructor(props) {
+    this.props = props;
+  }
+  render() {
+    return (
+      <div>
+        {this.props.name}
+        {this.props.age}
+      </div>
+    );
+  }
+}
+
+<Man name="samurai" age={31} />;
+
+// React видит что это class Man {...} и делает следущее
+
+let m1 = new Man("John", 24);
+let jsx = m1.render();
+//
