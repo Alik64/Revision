96a12810c1c9d58e9df517d7ef31ac261ce94e73
Albert <albert.barsamov@gmail.com>
2021-11-21 23:35:39 +0100

    // sum(1)(2) = 3

--- LearnJS/closure.js
+++ LearnJS/closure.js
@@ -47,3 +47,14 @@ if (true) {
 }
 
 sayHi(); // error
+
+// sum(1)(2) = 3
+// sum(5)(-1) = 4
+
+function sum(a) {
+
+    return function (b) {
+        return a + b; // берёт "a" из внешнего лексического окружения
+    };
+
+}
