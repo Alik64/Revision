d690834480afb3ffee8374deb932dfe9969dc76c
Albert <albert.barsamov@gmail.com>
2021-11-21 23:33:10 +0100

    objet + couter

---
+++ LearnJS/closure.js
@@ -0,0 +1,37 @@
+
+//1
+function makeCounter() {
+    let count = 0;
+
+    return function () {
+        return count++;
+    };
+}
+
+let counter = makeCounter();
+let counter2 = makeCounter();
+
+console.log(counter()); // 0
+console.log(counter()); // 1
+
+console.log(counter2()); // 0
+console.log(counter2()); // 1
+
+// 2
+function Counter() {
+    let count = 0;
+
+    this.up = function () {
+        return ++count;
+    };
+    this.down = function () {
+        return --count;
+    };
+}
+
+let counter = new Counter();
+
+alert(counter.up()); // 1
+alert(counter.up()); // 2
+alert(counter.down()); // 1
+
