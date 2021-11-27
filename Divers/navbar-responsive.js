50cd4ca390cb563062378f9a68ca5000c4adebbd
Albert <albert.barsamov@gmail.com>
2021-11-27 23:55:57 +0100

    menuMobil.addEventListener

--- Divers/navbar-responsive.js
+++ Divers/navbar-responsive.js
@@ -1,3 +1,9 @@
 const menuMobil = document.querySelector(".hamburger"); // bouton de menu mobile
 
 const nav = document.querySelector(".nav"); // menu avec des liens
+const lien = document.querySelectorAll("a.lien"); // [] contenant chaque (<a>) avec le class "lien"
+
+// A chaque fois qu'on appuie sur le menuMobil
+menuMobil.addEventListener("click", function () {
+    nav.classList.toggle("mobile");
+});
