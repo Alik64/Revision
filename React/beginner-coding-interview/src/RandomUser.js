bd2080c2f84d78cccc79c374f6d07422f3f843e0
Albert <albert.barsamov@gmail.com>
2021-12-17 22:34:28 +0100

    React: Add new user

--- React/beginner-coding-interview/src/RandomUser.js
+++ React/beginner-coding-interview/src/RandomUser.js
@@ -1,52 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const getUserName = (userInfo) => {
+    const { name: { first, last } } = userInfo
+    return `${first} ${last}`;
+}
+const getUserPhoto = (userInfo) => {
+    const { picture: { thumbnail } } = userInfo
+    return `${thumbnail}`
+}
+const getRandomUser = (pageNumber) => {
+    //?page=2
+    return axios
+        .get(`https://randomuser.me/api?page=${pageNumber}`)
+        .then(({ data }) => {
+            return data
+        })
+
+        .catch(err => {
+
+            return err
+        })
+}
+
 
 export default function RandomUser() {
 
 
 
     const [userInfo, setUserInfo] = useState([])
+    const [nextPageNumber, setNextPageNumber] = useState(1)
+    const addUser = () => {
+        getRandomUser(nextPageNumber).then((data) => {
+            if (data === undefined) return
+            const newResults = [
+                ...userInfo,
+                ...data.results
+            ]
 
-    const getRandomUser = () => {
-        return axios
-            .get('https://randomuser.me/api')
-            .then(({ data }) => {
-                console.log(data)
-                return data
-            }).then(data => {
-                setUserInfo(data.results)
-                console.log(userInfo)
-            })
-
-            .catch(err => {
-
-                console.log(err)
-            })
+            setUserInfo(newResults)
+            setNextPageNumber(data.info.page + 1)
+            console.log("userInfo", userInfo)
+        })
     }
 
 
 
-    const getUserName = (userInfo) => {
-        const { name: { first, last } } = userInfo
-        return `${first} ${last}`;
-    }
-    const getUserPhoto = (userInfo) => {
-        const { picture: { large } } = userInfo
-        return `${large}`
-    }
-    console.log(userInfo)
-
+    useEffect(() => {
+        addUser()
+    }, [])
 
 
 
     return (
         <div>
+            <button onClick={() => addUser()}>Add user</button>
             {
                 userInfo.map((data, index) => (
                     <div key={index}>
                         <img src={getUserPhoto(data)} alt="" />
                         <p>{getUserName(data)}</p>
+                        <p>{userInfo.info}</p>
                     </div>
                 )
                 )
@@ -55,7 +69,8 @@ export default function RandomUser() {
             }
 
 
-            <button onClick={() => getRandomUser()}>Click me</button>
+
+
         </div>
     )
 }
