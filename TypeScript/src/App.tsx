bb539040b2f3481e76c6669b21bcd15ed6e10563
Albert <albert.barsamov@gmail.com>
2022-01-01 21:57:05 +0100

    TypeScript : Types + arrays+basics+ obj

--- TypeScript/src/App.tsx
+++ TypeScript/src/App.tsx
@@ -2,6 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// ______ BAsic ______
+ 
+let a : number | null = 10;// soit number, soit null
+let name : string = "Happy new year"
+let sammuray : boolean | null = true; // soit boolean, soit null
+sammuray = null;
+
+//---- Arrays -----//
+
+let names : Array<string> =['Jack', "Marie", " Neo"]
+let names2 : string [] =['Jack', "Marie", " Neo"] // 2ème methode
+console.log(names[0].toUpperCase())
+
+// ____ STRING ___ //
+
+let sex: "male" | "female"
+sex="male" 
+// sex="man" // erreur
+
+//______Objects______//
+type AdressType = {
+  city: string
+  country: string
+}
+
+type UserType = {
+  sayHello: Function
+  name : string
+  age: number
+  isSamuray: boolean
+  adress: AdressType | null
+}
+let user:UserType ={
+  sayHello(message:string) {console.log('yo')},
+  name: "Neo",
+  age: 33,
+  isSamuray: true,
+  adress: {
+    city: "NewYork",
+    country: "USA"
+  }
+}
+console.log(user.name)
+
+
 function App() {
   return (
     <div className="App">
