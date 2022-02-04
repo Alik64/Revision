1c36adb72bd93355985eeadec848cccb5353a286
Albert <albert.barsamov@gmail.com>
2022-02-04 15:23:54 +0100

    useReducer

--- React/beginner-coding-interview/src/App.js
+++ React/beginner-coding-interview/src/App.js
@@ -1,24 +1,13 @@
 
 import './App.css';
-import Counter from './Counter';
-import RandomUser2 from './Intermediate/RandomUser2';
-import RandomUser from './RandomUser';
-import Starwars from './Starwars/Starwars';
 
-import Todo from './Todo/Todo';
-import TodoApp from './Todo2/TodoApp';
-import TodoLocalStorage from './TodoLocalStorage/TodoLocalStorage';
+import Hooks from './Hooks/Hooks';
+
 
 function App() {
   return (
     <div>
-      {/* <Counter />
-      <RandomUser /> */}
-      {/* <Todo /> */}
-      {/* <TodoApp /> */}
-      {/* <TodoLocalStorage /> */}
-      {/* <Starwars /> */}
-      <RandomUser2 />
+      <Hooks />
     </div>
 
   );
--- React/beginner-coding-interview/src/Hooks/Hooks.jsx
+++ React/beginner-coding-interview/src/Hooks/Hooks.jsx
@@ -2,14 +2,31 @@ import React, { useReducer, useState } from 'react';
 
 export default function Hooks() {
     const [counter, setCounter] = useState(0)
-    const [state, dispatch] = useReducer(first, second, third);
+    const [state, dispatch] = useReducer(reducer, initState);
     return (
         <div style={{
             display: "grid",
             placeItems: "center"
         }}>
             <h2>Vous avez cliqué {counter} fois</h2>
-
             <button onClick={() => setCounter(counter + 1)}>CLiquez ici</button>
+
+            <h2>useReducer count: {state.count}</h2>
+            <button onClick={() => dispatch({ type: "PLUS" })}>+</button>
+            <button onClick={() => dispatch({ type: "MINUS" })}>-</button>
         </div>)
+}
+
+// useReducer
+const initState = {
+    count: 0
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "PLUS":
+            return { count: state.count + 1 }
+        case "MINUS":
+            return { count: state.count - 1 }
+    }
 }
