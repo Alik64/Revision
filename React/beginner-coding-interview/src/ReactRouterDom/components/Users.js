be86ee2e93f79f0bd77888fb31e295458caf5961
Albert <albert.barsamov@gmail.com>
2022-03-07 16:26:44 +0100

    React: Search params

--- React/beginner-coding-interview/src/ReactRouterDom/components/Users.js
+++ React/beginner-coding-interview/src/ReactRouterDom/components/Users.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom'
 export const Users = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active'
     return (
         <div>
             <nav>
@@ -9,6 +12,10 @@ export const Users = () => {
                 <NavLink to='3'>User 3</NavLink>
             </nav>
             <Outlet />
+            <button onClick={() => setSearchParams({ filter: 'active' })}>Active users</button>
+            <button onClick={() => setSearchParams({})}>Reset Filter</button>
+
+            {showActiveUsers ? <h2>Showing active users</h2> : <h2>Showing All users </h2>}
         </div>
     )
 }
