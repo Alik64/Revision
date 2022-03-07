a6fe255db47a50db4da64b56396398ee30db4025
Albert <albert.barsamov@gmail.com>
2022-03-07 13:15:42 +0100

    React: Routes + LINK

--- React/beginner-coding-interview/src/App.js
+++ React/beginner-coding-interview/src/App.js
@@ -1,15 +1,22 @@
 
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-
-import Quiz from './ReactQuiz/Quiz';
+import About from './ReactRouterDom/pages/About';
+import Home from './ReactRouterDom/pages/Home';
 
 
 function App() {
   return (
-    <div>
-      <Quiz />
-    </div>
+    <>
+      <Link to='/'>Home</Link>
+      <Link to='about'>About</Link>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </>
 
   );
 }
