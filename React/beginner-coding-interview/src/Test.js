88181b6a7865006ce0cb7fba3a5b6a4e648c9b7f
Albert <albert.barsamov@gmail.com>
2022-03-09 17:08:49 +0100

    Test: dossier complet de tests

---
+++ React/beginner-coding-interview/src/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Test() {
+    return (
+        <>
+            <div>Hello world</div>
+            <button>Click me</button>
+            <input type="text" />
+        </>
+
+    )
+}
