83f2d571ef7db91e1aad7a3a1e5bb54e283611b9
Albert Barsamov <albert.barsamov@gmail.com>
2022-04-07 15:16:15 +0200

    GraphQl : One to Many query

--- GraphQl/e-commerce/index.js
+++ GraphQl/e-commerce/index.js
@@ -9,6 +9,7 @@ const products = [
         price: 42.44,
         image: "img-1",
         onSale: false,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70"
     },
     {
         id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
@@ -18,6 +19,8 @@ const products = [
         price: 53.5,
         image: "img-2",
         onSale: false,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70"
+
     },
     {
         id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
@@ -27,6 +30,8 @@ const products = [
         price: 1.33,
         image: "img-3",
         onSale: true,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70"
+
     },
     {
         id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
@@ -36,6 +41,8 @@ const products = [
         price: 332,
         image: "img-4",
         onSale: false,
+        categoryId: "34115aac-0ff5-4859-8f43-10e8db23602b"
+
     },
     {
         id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
@@ -45,6 +52,8 @@ const products = [
         price: 23.11,
         image: "img-5",
         onSale: true,
+        categoryId: "34115aac-0ff5-4859-8f43-10e8db23602b"
+
     },
     {
         id: "f01bcdec-6783-464e-8f9e-8416830f7569",
@@ -54,6 +63,8 @@ const products = [
         price: 59.99,
         image: "img-6",
         onSale: true,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d"
+
     },
     {
         id: "a4824a31-5c83-42af-8c1b-6e2461aae1ef",
@@ -63,6 +74,8 @@ const products = [
         price: 427.44,
         image: "img-7",
         onSale: false,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d"
+
     },
     {
         id: "b553085a-a7e0-4c9b-8a12-f971919c3683",
@@ -72,6 +85,8 @@ const products = [
         price: 77.0,
         image: "img-8",
         onSale: true,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d"
+
     },
     {
         id: "47bf3941-9c8b-42c0-9c72-7f3985492a5b",
@@ -81,6 +96,7 @@ const products = [
         price: 93.44,
         image: "img-9",
         onSale: false,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d"
     },
 ];
 const categories = [
@@ -123,6 +139,7 @@ type Product {
 type Category {
     id:ID!
     name: String!
+    products:[Product!]!
     }
 `
 const resolvers = {
@@ -140,6 +157,12 @@ const resolvers = {
             return categories.find(cat => cat.id === id)
         }
     },
+    Category: {
+        products: (parent, args, context) => {
+            const { id } = parent
+            return products.filter(prod => prod.categoryId === id)
+        }
+    },
 
 
 }
@@ -150,6 +173,3 @@ server.listen().then(({ url }) => {
     console.log('Server is ready at ' + url)
 })
 
-// server.listen().then(response => {
-//     console.log(response)
-// })
