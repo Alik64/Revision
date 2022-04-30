065c83f27cf87459cc01c93d1605f07317c1476b
Alik64 <albert.barsamov@gmail.com>
2022-04-30 16:02:18 +0200

    exemple3

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -17,19 +17,53 @@
 //     return;
 //   })
 //   .then();
+//-------------------------------//--------------------------------//
 
-// q
-console.log("Mi sidim na divane");
-const q = new Promise((resolve, reject) => {
-  console.log("Vasya sxodi v magazin!");
-  let isMilk = Math.floor(Math.random() * 10);
-  console.log("isMilk =", isMilk);
-  setTimeout(() => {
-    if (isMilk % 2 === 0) {
-      resolve("Moloko kupil!");
-    } else {
-      reject("Moloka ne bilo");
-    }
-  }, 2000);
-});
-console.log("Zakonchili... ");
+// const fn = (cb) => {
+//   return cb();
+// };
+
+// const a = fn(() => {
+//   console.log("a");
+// });
+//-------------------------------//--------------------------------//
+
+// console.log("Mi sidim na divane");
+// const q = new Promise((resolve, reject) => {
+//   console.log("Vasya sxodi v magazin!");
+//   let isMilk = Math.floor(Math.random() * 10);
+//   setTimeout(() => {
+//     if (isMilk % 2) {
+//       resolve("Ya smog kupit 1l moloka!");
+//     } else {
+//       reject("Ya ne smog kupit moloka");
+//     }
+//   }, 2000);
+// });
+// q.then((answer) => {
+//   console.log("Nu kak, ti kupil moloko?");
+//   console.log(answer);
+// }).catch((err) => {
+//   console.log("Nu kak, ti kupil moloko?");
+//   console.log(err);
+// });
+
+//-------------------------------//--------------------------------//
+
+console.log("start");
+setTimeout(() => console.log("setTimeout 1"), 0);
+Promise.resolve().then(() => console.log("Promise 1"));
+Promise.resolve().then(() => setTimeout(() => console.log("setTimeout 2"), 0));
+Promise.resolve().then(() => console.log("Promise 2"));
+setTimeout(() => console.log("setTimeout 3"), 0);
+console.log("finish");
+
+/*
+start
+finish
+Promise 1
+Promise 2
+setTimeout 1
+setTimeout 3
+setTimeout 2
+*/
