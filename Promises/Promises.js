4b28a7ada5578cbbb92ca1888c64f1ccee8c4e64
Alik64 <albert.barsamov@gmail.com>
2022-04-30 16:13:45 +0200

    ex 4

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -17,7 +17,7 @@
 //     return;
 //   })
 //   .then();
-//-------------------------------//--------------------------------//
+//-------------------------------/ 1 /--------------------------------//
 
 // const fn = (cb) => {
 //   return cb();
@@ -26,7 +26,7 @@
 // const a = fn(() => {
 //   console.log("a");
 // });
-//-------------------------------//--------------------------------//
+//-------------------------------/ 2 /--------------------------------//
 
 // console.log("Mi sidim na divane");
 // const q = new Promise((resolve, reject) => {
@@ -48,15 +48,15 @@
 //   console.log(err);
 // });
 
-//-------------------------------//--------------------------------//
+//-------------------------------/ 3 /--------------------------------//
 
-console.log("start");
-setTimeout(() => console.log("setTimeout 1"), 0);
-Promise.resolve().then(() => console.log("Promise 1"));
-Promise.resolve().then(() => setTimeout(() => console.log("setTimeout 2"), 0));
-Promise.resolve().then(() => console.log("Promise 2"));
-setTimeout(() => console.log("setTimeout 3"), 0);
-console.log("finish");
+// console.log("start");
+// setTimeout(() => console.log("setTimeout 1"), 0);
+// Promise.resolve().then(() => console.log("Promise 1"));
+// Promise.resolve().then(() => setTimeout(() => console.log("setTimeout 2"), 0));
+// Promise.resolve().then(() => console.log("Promise 2"));
+// setTimeout(() => console.log("setTimeout 3"), 0);
+// console.log("finish");
 
 /*
 start
@@ -66,4 +66,43 @@ Promise 2
 setTimeout 1
 setTimeout 3
 setTimeout 2
+*/
+
+//-------------------------------/ 4 /--------------------------------//
+console.log("START");
+
+setTimeout(() => console.log("setTimeout 1"), 0);
+
+const q = new Promise((resolve) => {
+  console.log("Promise 1");
+  setTimeout(() => console.log("setTimeout 2"));
+  resolve();
+});
+
+for (let i = 0; i < 3; i++) {
+  q.then(() => setTimeout(() => console.log("setTimeout 3")));
+}
+q.then(() => console.log("Promise 2"));
+
+for (let i = 0; i < 5; i++) {
+  q.then(() => console.log("Promise 3"));
+}
+
+console.log("finish");
+
+/*
+START
+Promise 1
+finish
+Promise 2
+Promise 3
+Promise 3
+Promise 3
+Promise 3
+Promise 3
+setTimeout 1
+setTimeout 2
+setTimeout 3
+setTimeout 3
+setTimeout 3
 */
