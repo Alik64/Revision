2005789dcb7e0b064866a261167b5b5ae2d102f4
Alik64 <albert.barsamov@gmail.com>
2022-05-01 12:01:38 +0200

    bubbleSort.js

---
+++ FCC/bubbleSort.js
@@ -0,0 +1,15 @@
+const bubbleSort = (arr) => {
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+let tab = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+console.log(bubbleSort(tab));
+
+// [1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643];
