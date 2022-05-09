8cd5171c117bd340bc7519e0e9d00b8051d0b0f8
Alik64 <albert.barsamov@gmail.com>
2022-05-09 23:37:25 +0200

    use ref

--- React/exercices/src/Hooks/UseRefHook.js
+++ React/exercices/src/Hooks/UseRefHook.js
@@ -1,19 +1,24 @@
 import React, { useRef, useState } from "react";
 
 const UseRefHook = () => {
+  const [width, setWidth] = useState(10);
   const [text, setText] = useState("");
-
-  const handleChange = () => {};
+  const box = useRef(null);
+  const inputText = useRef(null);
+  const handleChangeWidth = () => {
+    setWidth(box.current.offsetWidth + 5);
+  };
 
   const handleTextInputText = (event) => {
     setText(event.target.value);
   };
   return (
     <div>
-      <button onClick={handleChange}>change box width</button>
-      <div>
-        <input type="text" onChange={handleTextInputText} />
+      <button onClick={handleChangeWidth}>change box width</button>
+      <div ref={box} style={{ background: "darkblue", width }}>
+        BOX
       </div>
+      <input type="text" ref={inputText} onChange={handleTextInputText} />
       <div>{text}</div>
     </div>
   );
