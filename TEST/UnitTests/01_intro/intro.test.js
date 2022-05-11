327c1280bfc157630fbe8ab228bbf0987820c6c4
Alik64 <albert.barsamov@gmail.com>
2022-05-11 13:51:51 +0200

    MAtchers

--- TEST/UnitTests/01_intro/intro.test.js
+++ TEST/UnitTests/01_intro/intro.test.js
@@ -1,6 +1,27 @@
 const { sum, nativeNull } = require("./intro");
+describe("Sum function", () => {
+  test("Should return sum of two values", () => {
+    expect(sum(1, 3)).toBe(4);
+    expect(sum(1, 3)).toEqual(4);
+  });
 
-test("Sum should return sum of two values", () => {
-  expect(sum(1, 3)).toBe(4);
-  expect(sum(1, 3)).toEqual(4);
+  test("Should return value comparing to others", () => {
+    expect(sum(2, 3)).toBeGreaterThan(4);
+    expect(sum(2, 3)).toBeLessThan(6);
+    expect(sum(2, 3)).toBeLessThanOrEqual(5);
+  });
+
+  test("Should return sum of 2 float value", () => {
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+});
+describe("nativeNull function", () => {
+  test("Should return false tpo value null", () => {
+    expect(nativeNull()).toBe(null);
+    expect(nativeNull()).toBeNull();
+    expect(nativeNull()).toBeFalsy(); // undefined , null , 0 , ''
+    expect(nativeNull()).toBeDefined();
+    expect(nativeNull()).not.toBeTruthy();
+    expect(nativeNull()).not.toBeUndefined();
+  });
 });
