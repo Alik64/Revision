1a37364c3aff959f7f62780d985e8fc33842d68a
Alik64 <albert.barsamov@gmail.com>
2022-05-16 16:35:46 +0200

    validateValue test

--- TEST/ULBI/validateValue/validateValue.js
+++ TEST/ULBI/validateValue/validateValue.js
@@ -1,1 +1,6 @@
+const validateValue = (value) => {
+  if (value < 0 || value > 100) return false;
+  return true;
+};
 
+module.exports = validateValue;
--- TEST/ULBI/validateValue/validateValue.test.js
+++ TEST/ULBI/validateValue/validateValue.test.js
@@ -1,1 +1,19 @@
+const validateValue = require("./validateValue");
 
+describe("validateValue", () => {
+  test("smallest limit ", () => {
+    expect(validateValue(0)).toBe(true);
+  });
+  test("Biggest limit ", () => {
+    expect(validateValue(100)).toBe(true);
+  });
+  test("Valide number ", () => {
+    expect(validateValue(50)).toBe(true);
+  });
+  test("Smaller than limit ", () => {
+    expect(validateValue(-1)).toBe(false);
+  });
+  test("Bigger than limit ", () => {
+    expect(validateValue(101)).toBe(false);
+  });
+});
