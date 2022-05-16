08421dc11b7628468387532c75719aa90f587fa3
Alik64 <albert.barsamov@gmail.com>
2022-05-16 14:15:33 +0200

    test of groupBy

--- TEST/UnitTests/02_sync/sync.spec.js
+++ TEST/UnitTests/02_sync/sync.spec.js
@@ -32,4 +32,25 @@ describe("Lodash: groupBy", () => {
     expect(_.groupBy()).toBeDefined();
     expect(_.groupBy()).not.beUndefined();
   });
+
+  test("should gorup by Math.floor", () => {
+    const array = [2.2, 2.4, 4.2, 3.1];
+    const result = {
+      2: [2.2, 2.4],
+      4: [4.2],
+      3: [3.1],
+    };
+    expect(_.groupBy(array, Math.floor)).toEqual(result);
+  });
+  test("should gorup by lenth", () => {
+    const array = ["one", "two", "three"];
+    const result = {
+      5: ["three"],
+      3: ["one", "two"],
+    };
+    expect(_.groupBy(array, "length")).toEqual(result);
+  });
+  test("should NOT return an ARRAY", () => {
+    expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array);
+  });
 });
