ec061d5cdf93598e08b158e7f5a5954aaed3cd63
Alik64 <albert.barsamov@gmail.com>
2022-05-19 10:29:10 +0200

    element testing

--- TEST/react-tests/src/App.test.js
+++ TEST/react-tests/src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App/>',()=>{
+  test('renders my elements', () => {
+    render(<App />);
+    const myHeading = screen.getByText(/React testing/i);
+    const btn = screen.getByRole('button')
+    const input = screen.getByPlaceholderText(/hello/i)
+    expect(myHeading).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(btn).toBeInTheDocument()
+    screen.debug()
+    expect(input).toMatchSnapshot()
+  });
+  test('testing queryByText()', ()=>{
+    const hwElem = screen.queryByText(/hello world/i)
+    expect(hwElem).toBeNull()
+  })
+
+  test('testing async with findBy',()=>{
+    const myAsyncElem = screen.findByText(/hello/i)
+    
+  })
+})
+
+ 
