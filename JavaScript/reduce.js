a545988a33f082fe439f7ca483da4e2477c5886d
Alik64 <albert.barsamov@gmail.com>
2022-05-22 20:27:05 +0200

    reduce, people group by age

--- JavaScript/reduce.js
+++ JavaScript/reduce.js
@@ -18,3 +18,17 @@ const totalPrice = items.reduce((total, item) => {
 console.log("Total: ", totalPrice); // 135
 
 // --------------------------- EX 2 ----------------------- //
+const people = [
+  { name: "Kyle", age: 26 },
+  { name: "John", age: 31 },
+  { name: "Sally", age: 42 },
+  { name: "Jill", age: 42 },
+];
+
+const result = people.reduce((groupedPeople, person) => {
+  const age = person.age;
+  if (groupedPeople[age] == null) groupedPeople[age] = [];
+  groupedPeople[age].push(person);
+  return groupedPeople;
+}, {});
+console.log(result);
