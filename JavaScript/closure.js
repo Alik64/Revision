5bba1df42278d08734c8bb3e8170c9bc937be1d5
Alik64 <albert.barsamov@gmail.com>
2022-05-23 16:30:22 +0200

    url generator

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,43 +1,57 @@
-function parent(number) {
-  return function child() {
-    console.log(number);
-  };
-}
-const cmon = parent(42);
-cmon();
+// function parent(number) {
+//   return function child() {
+//     console.log(number);
+//   };
+// }
+// const cmon = parent(42);
+// cmon();
 // --------------------------- EX 1 ----------------------- //
 
-function foo() {
-  var a = 2;
-
-  function bar() {
-    console.log(a);
-  }
-
-  return bar;
+function urlGenerator(domain) {
+  return function (url) {
+    return `https://${url}.${domain}`;
+  };
 }
 
-var baz = foo();
+const comUrl = urlGenerator("com");
+console.log(comUrl("google")); //https://google.com
 
-baz(); // 2 -- Ого, замыкание только что было раскрыто, мужики!
+const frUrl = urlGenerator("fr");
+console.log(frUrl("yahoo")); //https://yahoo.com
+
+// --------------------------- EX 1 ----------------------- //
+
+// function foo() {
+//   var a = 2;
+
+//   function bar() {
+//     console.log(a);
+//   }
+
+//   return bar;
+// }
+
+// var baz = foo();
+
+// baz(); // 2 -- Ого, замыкание только что было раскрыто, мужики!
 
 // --------------------------- EX 2 ----------------------- //
 
-function wait(message) {
-  setTimeout(function timer() {
-    console.log(message);
-  }, 1000);
-}
+// function wait(message) {
+//   setTimeout(function timer() {
+//     console.log(message);
+//   }, 1000);
+// }
 
-wait("Привет, замыкание!");
+// wait("Привет, замыкание!");
 
 // --------------------------- EX 2 ----------------------- //
 
-for (var i = 1; i <= 5; i++) {
-  (function () {
-    var j = i;
-    setTimeout(function timer() {
-      console.log(j);
-    }, j * 1000);
-  })();
-}
+// for (var i = 1; i <= 5; i++) {
+//   (function () {
+//     var j = i;
+//     setTimeout(function timer() {
+//       console.log(j);
+//     }, j * 1000);
+//   })();
+// }
