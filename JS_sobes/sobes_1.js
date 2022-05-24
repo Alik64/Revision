7535a499d106b2ef79898dc841d3b76b70188f91
Alik64 <albert.barsamov@gmail.com>
2022-05-24 17:47:20 +0200

    sobes1

---
+++ JS_sobes/sobes_1.js
@@ -0,0 +1,29 @@
+/*
+
+let arr = [0, 1, 2];
+console.log(Array.isArray(arr));
+
+console.log((5 && 2) || 3); // 2
+console.log(2 && 3); // 3
+console.log((2 && 3) || 5); // 3
+console.log((2 || 3) && 5); // 5
+
+// [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+function arrDuplicator(arr) {
+  //   return [...arr, ...arr];
+  return arr.concat(arr);
+}
+
+console.log(arrDuplicator([1, 2, 3, 4, 5]));
+
+let x = 7;
+console.log(x);
+console.log(0.1 + 0.2 - 0.2 == 0.1); // 
+*/
+
+function f1(a, b) {
+  console.log(a, b);
+}
+let f2 = f1.bind(null, "foo");
+
+f2("bar", "baz");
