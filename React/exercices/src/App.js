a6ca945924bb173ceba4547ba12cba81247af737
Alik64 <albert.barsamov@gmail.com>
2022-06-01 22:24:45 +0200

    CG welcome.js

--- React/exercices/src/App.js
+++ React/exercices/src/App.js
@@ -1,34 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import Color from "./CG/Color";
-import { Counter } from "./CG/Counter";
-import Hello from "./CG/Hello";
-import { data } from "./CG/Hello";
-
-import Warning from "./CG/Warning";
-
-import UseContextHook from "./Hooks/UseContextHook";
 
 function App() {
   return (
     <>
-      <Result getNewResult={() => "texlxnfldjgnort"} />
+      <Welcome user="ALk" primary />
     </>
   );
 }
 
 export default App;
-
-function Result({ getNewResult }) {
-  const [result, setResult] = useState(null);
-
-  function handleClick() {
-    setResult(getNewResult());
-  }
-  return (
-    <div>
-      <button onClick={handleClick}>Click</button>
-      {result != null && <span>{result}</span>}
-    </div>
-  );
-}
