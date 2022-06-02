05ca6b5b245bd1c311f3c318c01d99d494add660
Alik64 <albert.barsamov@gmail.com>
2022-06-02 15:32:03 +0200

    CG Hex.js

--- React/exercices/src/CG/Hex.js
+++ React/exercices/src/CG/Hex.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 function Parent({ value }) {
-  return null;
+  return <Hex value={value} component={(props) => <Child {...props} />} />;
 }
 
 function Hex({ value, component }) {
-  return <div id="hexed"></div>;
+  let hexedValue = value.toString(16);
+  const Child = component;
+
+  return (
+    <div id="hexed">
+      <Child value={hexedValue} />
+    </div>
+  );
 }
 function Child({ value }) {
-  return <div id="child">{value}</div>;
+  return <div id="child">0x{value}</div>;
 }
 
-export default Hex;
+export default Parent;
