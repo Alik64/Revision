4f4b5aaa4be4beaf6aa4916e8e29610b7333ceeb
Alik64 <albert.barsamov@gmail.com>
2022-06-03 14:56:41 +0200

    add dynamicsTypes.js

---
+++ JavaScript/Basics/dynamicTypes.js
@@ -0,0 +1,22 @@
+//--------------------- Numbers ---------------------//
+
+console.log(Number("5")); // 5
+console.log(typeof +"5"); // number
+console.log(Number("str")); // NaN
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+null); // 0
+
+//--------------------- Boolean ---------------------//
+
+console.log(Boolean("")); // false
+console.log(Boolean(" ")); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(null)); // false
+console.log(!!"abc"); // true
+
+//--------------------- Ex ---------------------//
+
+console.log("" + 5); // '5'
+console.log(+"10"); // 10
+console.log(!!""); // false
