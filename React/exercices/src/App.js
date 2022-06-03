92eabca052e5150b46231ed7ea291be56eb6a6af
Alik64 <albert.barsamov@gmail.com>
2022-06-03 14:19:52 +0200

    userSort.js

--- React/exercices/src/App.js
+++ React/exercices/src/App.js
@@ -3,11 +3,16 @@ import "./App.css";
 import Communication from "./CG/Communication";
 import ListUsers from "./CG/ListUsers";
 import Welcome from "./CG/Welcome";
+const users = [
+  { id: 1, firstname: "Alexandre", lastname: "Zegrand" },
+  { id: 2, firstname: "Marie", lastname: "Rose" },
+  { id: 3, firstname: "India", lastname: "Loulou" },
+];
 
 function App() {
   return (
     <>
-      <Communication initial="test" />
+      <UsersSorts data={users} />
     </>
   );
 }
--- challenges/usersSort.js
+++ challenges/usersSort.js
@@ -1,11 +1,27 @@
 const users = [
-    { id: 1, firstname: 'Alexandre', lastname: 'Zegrand' },
-    { id: 2, firstname: 'Marie', lastname: 'Rose' },
-    { id: 3, firstname: 'India', lastname: 'Loulou' },
-]
+  { id: 1, firstname: "Alexandre", lastname: "Zegrand" },
+  { id: 2, firstname: "Marie", lastname: "Rose" },
+  { id: 3, firstname: "India", lastname: "Loulou" },
+];
 
-let usersSort = users.sort((a, b) => a.lastname < b.lastname ? -1 : 1)
-console.log(usersSort)
+let usersSort = users.sort((a, b) => (a.lastname < b.lastname ? -1 : 1));
+console.log(usersSort);
 
+import React from "react";
 
+const usersSorts = ({ data }) => {
+  const copyData = [...data];
+  return (
+    <div>
+      {copyData
+        .sort((a, b) => (a.lastname < b.lastname ? -1 : 1))
+        .map((user) => (
+          <div key={user.id}>
+            {user.firstname} {user.lastname}
+          </div>
+        ))}
+    </div>
+  );
+};
 
+export default usersSorts;
