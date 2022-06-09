81aad8e2aabeaec75fc62b07cb4af24cda31566c
Alik64 <albert.barsamov@gmail.com>
2022-06-09 10:50:41 +0200

    ListUsers update

--- React/exercices/src/App.js
+++ React/exercices/src/App.js
@@ -3,16 +3,21 @@ import "./App.css";
 import Communication from "./CG/Communication";
 import ListUsers from "./CG/ListUsers";
 import Welcome from "./CG/Welcome";
+// const users = [
+//   { id: 1, firstname: "Alexandre", lastname: "Zegrand" },
+//   { id: 2, firstname: "Marie", lastname: "Rose" },
+//   { id: 3, firstname: "India", lastname: "Loulou" },
+// ];
+
 const users = [
-  { id: 1, firstname: "Alexandre", lastname: "Zegrand" },
-  { id: 2, firstname: "Marie", lastname: "Rose" },
-  { id: 3, firstname: "India", lastname: "Loulou" },
+  { firstName: "Ada", lastName: "Lovelace" },
+  { firstName: "Donald", lastName: "Knuth" },
 ];
 
 function App() {
   return (
     <>
-      <UsersSorts data={users} />
+      <ListUsers users={users} />
     </>
   );
 }
--- React/exercices/src/CG/ListUsers.js
+++ React/exercices/src/CG/ListUsers.js
@@ -5,19 +5,23 @@ import React from "react";
 // ];
 const ListUsers = ({ users }) => {
   // const copyUsers = [...users];
+
   return (
     <>
-      <div className="user-count"> Users: {users.length}</div>
+      <div className="user-count"> Users: {!!users ? users.length : 0}</div>
       <ul className="user-list">
-        {users
-          .sort((a, b) => (a.lastName < b.lastName ? -1 : 1))
-          .map((user) => {
-            return (
-              <li>
-                {user.firstName} {user.lastName}
-              </li>
-            );
-          })}
+        {users &&
+          users
+            .sort((a, b) =>
+              a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1
+            )
+            .map((user) => {
+              return (
+                <li key={user.lastName} style={{ color: "white" }}>
+                  {user.firstName} {user.lastName}
+                </li>
+              );
+            })}
       </ul>
     </>
   );
