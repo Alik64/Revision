3a632f783531e37c3aef51b7ac2dfbcf1f3df18e
Alik64 <albert.barsamov@gmail.com>
2022-06-14 23:29:29 +0200

    reverseWordPosition

--- JavaScript/Algos/ReverseWordPosition.js
+++ JavaScript/Algos/ReverseWordPosition.js
@@ -1,6 +1,17 @@
-function reverse(str) {
-    return str.split(' ').reverse()
+/*
+INPUT:
+S, a string.
+OUTPUT:
+A string containing all the space-separated words of S in reverse order.
 
+CONSTRAINTS:
+S contains at least one word.
+S contains less than 1000 characters. */
+
+function reverse(str) {
+  if (str.length === 0 || str.length > 1000) return;
+  return str.split(" ").reverse().join(" ");
 }
 
-console.log(reverse('hello world'))
+console.log(reverse("hello world"));
+console.log('reverse("") ==>', reverse(""));
---
+++ JavaScript/Algos/userNameChecker.js
@@ -0,0 +1,18 @@
+/*
+You must write a program that checks to see if the given username U is valid
+
+A username must:
+- Have at least 3 characters
+- Have no more than 20 characters
+- Only consist of letters and digits [a-z] [A-Z] [0-9]
+
+If the username meets these requirements, print VALID
+If the username doesn't meet these requirements, print INVALID 
+
+Constraints
+Length of u < 64
+*/
+
+function checkString(str) {
+  if (str.length >= 64) return;
+}
