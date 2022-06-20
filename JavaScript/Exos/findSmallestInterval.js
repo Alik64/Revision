de745815b85155704b2b24019fbe94c8746c4fbe
Alik64 <albert.barsamov@gmail.com>
2022-06-20 23:46:08 +0200

    fs int v3

--- JavaScript/Exos/findSmallestInterval.js
+++ JavaScript/Exos/findSmallestInterval.js
@@ -5,7 +5,7 @@ ex: [1,6,4,2] => 1 car 2-1 = 1
 Tableau dois comporter au moins 2 éléments, et 100 000 max
 La solution doit être optimiser pour les tableaux de grands tailles. */
 
-let arr = [6, 1, 4, 2];
+let arr = [6, 1, 4, 2, 98];
 
 function findSmallestInterval(arr) {
   let sortedArr = arr.sort();
