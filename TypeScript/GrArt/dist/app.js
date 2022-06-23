86a02958de34c949e12a461e61c1a10bdd1752e3
Alik64 <albert.barsamov@gmail.com>
2022-06-23 22:50:52 +0200

    add tsconfig.json + use 'strict'

--- TypeScript/GrArt/dist/app.js
+++ TypeScript/GrArt/dist/app.js
@@ -1,1 +1,8 @@
 console.log("Hello mfrs!");
+const compteur = document.querySelector("#compteur");
+let i = 0;
+const increment = (e) => {
+    i++;
+    compteur.querySelector("span").innerText = i.toString();
+};
+compteur.addEventListener("click", increment);
--- TypeScript/GrArt/src/app.ts
+++ TypeScript/GrArt/src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello mfrs!");
+const compteur = document.querySelector("#compteur");
+
+let i = 0;
+
+const increment = (e) => {
+  i++;
+  compteur.querySelector("span").innerText = i.toString();
+};
+
+compteur.addEventListener("click", increment);
