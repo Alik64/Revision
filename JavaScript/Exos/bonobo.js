061d8ec9a429febfc66284764dd888f3d3c204f0
Alik64 <albert.barsamov@gmail.com>
2022-06-24 00:11:20 +0200

    ninjacode bonobo.js

--- JavaScript/Exos/bonobo.js
+++ JavaScript/Exos/bonobo.js
@@ -16,33 +16,34 @@ Example     Input      Output
             bonobo     bonoobbooo
 */
 
-function repeatLetters(str) {
-  let arrFromStr = str.split("");
+// function repeatLetters(str) {
+//   let arrFromStr = str.split("");
 
-  const count = {}; //{ b: 2, o: 3, n: 1 }
+//   const count = {}; //{ b: 2, o: 3, n: 1 }
 
-  arrFromStr.forEach((element) => {
-    count[element] = (count[element] ?? 0) + 1;
-  });
+//   arrFromStr.forEach((element) => {
+//     count[element] = (count[element] ?? 0) + 1;
+//   });
 
-  return count;
-}
+//   return count;
+// }
 
-function repeatLetters(str) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    result += str[i];
-    for (let j = 0; j < i; j++) {
-      if (str[i] === str[j]) {
-        result += str[i];
-      }
-    }
-  }
-  return result;
-}
+// function repeatLetters(str) {
+//   let result = "";
+//   for (let i = 0; i < str.length; i++) {
+//     result += str[i];
+//     for (let j = 0; j < i; j++) {
+//       if (str[i] === str[j]) {
+//         result += str[i];
+//       }
+//     }
+//   }
+//   return result;
+// }
 
+const s = (s) => s.replace((n = /./g), (c) => c.repeat((n[c] = -~n[c])));
 let str = "bonobo";
 let str2 = "repetitive";
 
-console.log("repeatLetters(str) =>", repeatLetters(str)); // bonoobbooo
-console.log("repeatLetters(str) =>", repeatLetters(str2)); // repeetittiiveee
+console.log("repeatLetters(str) =>", s(str)); // bonoobbooo
+console.log("repeatLetters(str) =>", s(str2)); // repeetittiiveee
