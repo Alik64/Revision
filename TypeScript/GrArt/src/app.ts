e1d29ecaea0487ae4ee2d71e0fd2b0c586962dc8
Alik64 <albert.barsamov@gmail.com>
2022-06-23 23:47:23 +0200

    Add Syntaxe de base + correction

--- TypeScript/GrArt/src/app.ts
+++ TypeScript/GrArt/src/app.ts
@@ -1,10 +1,53 @@
+/* Syntaxe de Base  */
+/* 
+const a: string = "Hello world!";
+const n: number = 13;
+const b: boolean = true;
+const d: null = null;
+const arr: string[] = ["ts", "is", "fun"];
+const arrAny: any[] = ["ts", 4, "fun", null, undefined, true];
+const user: { firstname: string; lastname: string } = {
+  firstname: "John",
+  lastname: "Doe",
+};
+const user2: { firstname: string; lastname?: string } = {
+  firstname: "John",
+};
+const user3: { firstname: string; [key: string]: string } = {
+  firstname: "John",
+  lastname: "Doe",
+  city: "Miami",
+};
+const date: Date = new Date();
+const cb: Function = (e: MouseEvent): void => {};
+const cb2: (e: MouseEvent) => void = (e: MouseEvent): void => {};
+const cb3: (e: MouseEvent) => void = (e: MouseEvent): number => {
+  return 3;
+};
+function printId(id: number): void {
+  console.log(id.toString());
+}
+
+const compteur = document.querySelector("#compteur") as HTMLButtonElement;
+const compteur2 = <HTMLButtonElement>document.querySelector("#compteur");
+
+function printId2(id: number | string): void {
+  console.log(id.toString());
+}
+
+*/
 const compteur = document.querySelector("#compteur");
 
 let i = 0;
 
-const increment = (e) => {
+const increment = (e: Event) => {
+  e.preventDefault();
   i++;
-  compteur.querySelector("span").innerText = i.toString();
+  const span = compteur?.querySelector("span");
+  if (span) {
+    span.innerText = i.toString();
+  }
 };
-
-compteur.addEventListener("click", increment);
+if (compteur) {
+  compteur.addEventListener("click", increment);
+}
