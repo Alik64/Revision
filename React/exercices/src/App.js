17fdace5d6823d1d1e53a1e9f90c00469b52eb8a
Alik64 <albert.barsamov@gmail.com>
2022-06-25 23:51:21 +0200

    test of triad API

--- React/exercices/src/App.js
+++ React/exercices/src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
-import Communication from "./CG/Communication";
-import ListUsers from "./CG/ListUsers";
-import Welcome from "./CG/Welcome";
+
 // const users = [
 //   { id: 1, firstname: "Alexandre", lastname: "Zegrand" },
 //   { id: 2, firstname: "Marie", lastname: "Rose" },
@@ -15,9 +14,41 @@ const users = [
 ];
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        "https://triple-triad-game.herokuapp.com/api/characters"
+      );
+      console.log(result.data);
+      setCharacters(result.data.data);
+    };
+    fetchData();
+  }, []);
+  console.log("characters ==>", characters);
   return (
     <>
-      <ListUsers />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gridTemplateRows: "repeat(3,1fr)",
+        }}
+      >
+        {characters.map((char) => (
+          <div key={char.id}>
+            <div>
+              <h1>{char.name}</h1>
+              <img
+                src={char.thumbnail.path}
+                alt=""
+                style={{ width: "150px" }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
