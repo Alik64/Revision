354213dc639e5274b767e1da142dfca988d975b0
Alik64 <albert.barsamov@gmail.com>
2022-07-04 22:51:52 +0200

    Redux : actionCreator

--- REDUX/main.js
+++ REDUX/main.js
@@ -1,1 +1,25 @@
 console.log("hello : ", "hello");
+
+let state = 0;
+
+const updateState = (state, { type, payload }) => {
+  if (type === "INC") {
+    return state + payload;
+  } else if (type === "DEC") {
+    return state - payload;
+  } else {
+    return state;
+  }
+};
+
+const incAction = (amount) => ({ type: "INC", payload: amount });
+const decAction = (amount) => ({ type: "DEC", payload: amount });
+
+state = updateState(state, incAction(2));
+console.log("state : ", state);
+state = updateState(state, decAction(5));
+console.log("state : ", state);
+state = updateState(state, decAction(7));
+console.log("state : ", state);
+state = updateState(state, {});
+console.log("state : ", state);
