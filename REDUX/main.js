21426a6589fbd1eb6178b255484343ad7ee6cfb0
Alik64 <albert.barsamov@gmail.com>
2022-07-04 23:23:13 +0200

    Redux : add preventing of state rewrite ( using SPREAD)

--- REDUX/main.js
+++ REDUX/main.js
@@ -3,10 +3,14 @@ let state = 0;
 const updateState = (state, { type, payload }) => {
   if (type === "INC") {
     return {
+      ...state,
       count: state.count + payload,
     };
   } else if (type === "DEC") {
-    return { count: state.count - payload };
+    return {
+      ...state,
+      count: state.count - payload,
+    };
   } else {
     return state;
   }
@@ -31,20 +35,22 @@ class Store {
 
 const initialState = {
   count: 0,
+  name: "John Doe",
 };
 const store = new Store(updateState, initialState);
+const { dispatch } = store;
 store.subscribe(() => console.log("---subscribe 1", store.getState().count));
 store.subscribe(() => console.log("---subscribe 2", store.getState()));
 
 const incAction = (amount) => ({ type: "INC", payload: amount });
 const decAction = (amount) => ({ type: "DEC", payload: amount });
 
-store.dispatch(incAction(5));
-store.dispatch(decAction(24));
-store.dispatch(incAction(10));
-store.dispatch(decAction(15));
-store.dispatch(incAction(2));
-store.dispatch(decAction(6));
+dispatch(incAction(5));
+dispatch(decAction(24));
+dispatch(incAction(10));
+dispatch(decAction(15));
+dispatch(incAction(2));
+dispatch(decAction(6));
 // console.log("store.getState() : ", store.getState());
 
 // state = updateState(state, incAction(2));
