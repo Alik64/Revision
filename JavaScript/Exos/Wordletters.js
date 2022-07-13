21372fb499cc018e1eba4c73273d38b94a77725e
Alik64 <albert.barsamov@gmail.com>
2022-07-13 17:01:43 +0200

    wordsLetter

--- JavaScript/Exos/Wordletters.js
+++ JavaScript/Exos/Wordletters.js
@@ -1,18 +1,25 @@
 function filterWords(words, letters) {
-    // Write your code here
-    // To debug: console.error('Debug messages...');
+  // Write your code here
+  // To debug: console.error('Debug messages...');
+  let arr = letters.split("");
+  let result = [];
 
-    let arr = letters.split('');
-    let result = [];
-
-    for (let i = 0; i < words.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (words[i].includes(arr[j])) {
-                result.push(words[i])
-            }
-        }
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (words[i].includes(arr[j]) && !result.includes(words[i])) {
+        result.push(words[i]);
+      }
     }
-    console.log(result)
+  }
+
+  for (let x = 0; x < result.length; x++) {
+    if (result[x] === result[x + 1]) {
+      result.splice(x, 1);
+      x--;
+    }
+  }
+  return result;
 }
 
-filterWords(['the', 'dog', 'got', 'a', 'bone'], "ae")
+console.log(filterWords(["the", "dog", "got", "a", "bone"], "ae"));
+console.log(filterWords(["he", "saw", "and", "she", "saw"], "hs"));
