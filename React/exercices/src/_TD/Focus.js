ebce5d9a5cc6aef493b6ac5cd1784ed89481ab82
Alik64 <albert.barsamov@gmail.com>
2022-07-18 23:38:56 +0200

    add Focus.js

---
+++ React/exercices/src/_TD/Focus.js
@@ -0,0 +1,34 @@
+// React is loaded and is available as React and ReactDOM
+// imports should NOT be used
+class Input extends React.PureComponent {
+  render() {
+    let { forwardedRef, ...otherProps } = this.props;
+    return <input {...otherProps} ref={forwardedRef} />;
+  }
+}
+
+const TextInput = React.forwardRef((props, ref) => {
+  return <Input {...props} forwardedRef={ref} />;
+});
+
+class FocusableInput extends React.Component {
+  ref = React.createRef();
+
+  render() {
+    return <TextInput ref={this.ref} />;
+  }
+
+  // When the focused prop is changed from false to true,
+  // and the input is not focused, it should receive focus.
+  // If focused prop is true, the input should receive the focus.
+  // Implement your solution below:
+  componentDidUpdate(prevProps) {
+    this.ref.current.focus();
+  }
+
+  componentDidMount() {
+    if (this.props.focused) {
+      this.ref.current.focus();
+    }
+  }
+}
