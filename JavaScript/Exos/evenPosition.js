3ad934e1c3b4995bd8498c2cb3698061e5533756
Alik64 <albert.barsamov@gmail.com>
2022-07-29 22:18:22 +0200

    oddPosition

--- JavaScript/Exos/evenPosition.js
+++ JavaScript/Exos/evenPosition.js
@@ -1,20 +1,16 @@
 function evenPosition(table, value) {
   let position = table.indexOf(value);
-  if (position === 0) {
-    return false;
-  }
+
   return position % 2 === 0 ? true : false;
 }
 function oddPosition(table, value) {
   let position = table.indexOf(value);
-  if (position === 0) {
-    return true;
-  }
+
   return position % 2 !== 0 ? true : false;
 }
 
 let arr = [4, 23, 45, 6, 5, 43, 23, 7, 65, 2, 1];
 console.log("evenPosition(arr,7) : ", evenPosition(arr, 65)); //true
-console.log("evenPosition(arr,4) : ", evenPosition(arr, 4)); //false
-console.log("oddPosition(arr,7) : ", oddPosition(arr, 65)); //false
+console.log("evenPosition(arr,4) : ", evenPosition(arr, 23)); //false
+console.log("oddPosition(arr,7) : ", oddPosition(arr, 2)); //false
 console.log("oddPosition(arr,4) : ", oddPosition(arr, 4)); //true
