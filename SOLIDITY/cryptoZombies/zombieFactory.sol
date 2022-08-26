a0112cda6330aac3e2c3c25385f734210bead534
Alik64 <albert.barsamov@gmail.com>
2022-08-26 23:34:03 +0200

    ZF: lesson 4

--- SOLIDITY/cryptoZombies/zombieFactory.sol
+++ SOLIDITY/cryptoZombies/zombieFactory.sol
@@ -4,4 +4,9 @@ pragma solidity 0.8.16;
 contract ZombieFactory {
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
 }
