92e253550097213fba1ac5a082eabcc303b24991
Alik64 <albert.barsamov@gmail.com>
2022-08-28 22:03:57 +0200

    FCC : fearNotLettersInitialisation

---
+++ FCC/fearNotLetter.js
@@ -0,0 +1,16 @@
+/*
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+Waiting:fearNotLetter("abce") should return the string d.
+Waiting:fearNotLetter("abcdefghjklmno") should return the string i.
+Waiting:fearNotLetter("stvwx") should return the string u.
+Waiting:fearNotLetter("bcdf") should return the string e.
+Waiting:fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
+*/
+
+function fearNotLetter(str) {
+  console.log(str.charAt(0));
+}
+
+console.log(fearNotLetter("abce")); //d
