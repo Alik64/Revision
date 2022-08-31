fae054d10b523b6e0b65bc39422fdfb6d58afb9d
Alik64 <albert.barsamov@gmail.com>
2022-08-31 23:29:02 +0200

    Chapitre 4: Require

---
+++ SOLIDITY/cryptoZombies/01_Solidity/02_ZombieAttack/00_zombieAttack.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
