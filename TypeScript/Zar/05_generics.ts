ce8571f7bdcdaba2c9309abf11d1d071e9ad1a9e
Alik64 <albert.barsamov@gmail.com>
2022-10-09 21:34:16 +0200

    add myArray generic

--- TypeScript/Zar/05_generics.ts
+++ TypeScript/Zar/05_generics.ts
@@ -5,4 +5,11 @@ function identity<T>(value: T): T {
 identity(1);
 identity("str");
 
-const tsArr4: Array<number> = [1, 2, 3, 4];
+interface MyArray<T> {
+  [n: number]: T;
+  map<U>(fn: (el: T) => U): U[];
+}
+const tsArr: Array<number> = [1, 2, 3, 4];
+const tsArr1: MyArray<number> = [1, 2, 3, 4];
+
+tsArr1.map((i) => `${i + 1}`);
