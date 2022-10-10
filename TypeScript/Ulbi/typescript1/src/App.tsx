0dc16d0a3f390c0f8055db0618ce1c7ddeb45b62
Alik64 <albert.barsamov@gmail.com>
2022-10-10 12:52:56 +0200

    Props typisation

---
+++ TypeScript/Ulbi/typescript1/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div>
+      <Card width="120px" height="120px">
+        <button>Click me</button>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
