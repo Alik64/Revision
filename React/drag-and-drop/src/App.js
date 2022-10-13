d083e55534941d6ceca57bdb638fe25739a4ae6f
Alik64 <albert.barsamov@gmail.com>
2022-10-13 18:29:50 +0200

    remove semicolon bug

--- React/drag-and-drop/src/App.js
+++ React/drag-and-drop/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   };
   return (
     <DragDropContext onDragEnd={onDragEndHandler}>
-      <div>{columns}</div>);
+      <div>{columns}</div>
     </DragDropContext>
   );
 };
