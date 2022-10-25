f6a5feb29b03ef4df95a9c4aaaaea450a550eaeb
Alik64 <albert.barsamov@gmail.com>
2022-10-25 10:25:22 +0200

    update JS soluiton

--- CG/_cg/stocks.js
+++ CG/_cg/stocks.js
@@ -1,48 +1,31 @@
-function average(a, n) {
-  var sum = 0;
-  for (var i = 0; i < n; i++) sum += a[i];
-
-  return parseFloat(sum / n);
-}
-
 function stocksRank(stocks, prices) {
-  let priceByStock = [];
-
+  let averages = [];
   const stocksObj = {};
 
   for (let i = 0; i < stocks.length; i++) {
-    prices.forEach((arr) => {
-      priceByStock.push(arr[i]);
-    });
+    let sum = 0;
+    let average = 0;
+    prices.forEach((price) => (sum += price[i]));
+    average = sum / prices.length;
+    averages.push(average);
   }
 
-  let priceByStockMatrix = priceByStock.reduce(function (rows, key, index) {
-    return (
-      (index % prices.length == 0
-        ? rows.push([key])
-        : rows[rows.length - 1].push(key)) && rows
-    );
-  }, []);
-
-  let averageArr = priceByStockMatrix.map((arr) => average(arr, arr.length));
-
   stocks.forEach((element, index) => {
-    stocksObj[element] = averageArr[index];
+    stocksObj[element] = averages[index];
   });
+  console.log(stocksObj);
 
   let topStocks = Object.keys(stocksObj).sort(function (a, b) {
     return stocksObj[b] - stocksObj[a];
   });
-
   return topStocks.slice(0, 3);
 }
 
-let stocks = ["AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA", "AAA"];
+let stocks = ["AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA"];
 let prices = [
-  [12.81, 11.09, 12.11, 10.93, 9.83, 8.14, 20],
-  [10.34, 10.56, 10.14, 12.17, 13.1, 11.22, 20],
-  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21, 20],
-  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21, 20],
+  [12.81, 11.09, 12.11, 10.93, 9.83, 8.14],
+  [10.34, 10.56, 10.14, 12.17, 13.1, 11.22],
+  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21],
 ];
 
 console.log(stocksRank(stocks, prices));
