c01012c24053bd67099fa0e15fad6ee796d5eab1
Alik64 <albert.barsamov@gmail.com>
2022-10-25 10:38:14 +0200

    update TS soluiton

--- CG/_cg/stocksTs.ts
+++ CG/_cg/stocksTs.ts
@@ -2,54 +2,34 @@ type StockObjType = {
   [index: string]: number;
 };
 
-function averageTs(arr: number[], n: number): any {
-  let sum: number = 0;
-  for (let i = 0; i < n; i++) sum += arr[i];
-
-  return Math.round((sum / n) * 100) / 100;
-}
-
-function stocksRankTs(stocks: string[], prices: number[][]): string[] {
-  let priceByStock: number[] = [];
-
+function stocksRankTs(stocks: string[], prices: number[][]) {
+  let averages: number[] = [];
   const stocksObj: StockObjType = {};
 
   for (let i = 0; i < stocks.length; i++) {
-    prices.forEach((arr) => {
-      priceByStock.push(arr[i]);
-    });
+    let sum = 0;
+    let average = 0;
+    prices.forEach((price) => (sum += price[i]));
+    average = sum / prices.length;
+    averages.push(average);
   }
-  console.log(priceByStock);
-  let priceByStockMatrix: number[][] = priceByStock.reduce(
-    (rows, key, index) => {
-      return (
-        (index % prices.length == 0
-          ? rows.push([key])
-          : rows[rows.length - 1].push(key)) && rows
-      );
-    },
-    []
-  );
-
-  let averageArr = priceByStockMatrix.map((arr) => averageTs(arr, arr.length));
 
   stocks.forEach((element, index) => {
-    stocksObj[element] = averageArr[index];
+    stocksObj[element] = averages[index];
   });
+  console.log(stocksObj);
 
   let topStocks = Object.keys(stocksObj).sort(function (a, b) {
     return stocksObj[b] - stocksObj[a];
   });
-
   return topStocks.slice(0, 3);
 }
 
-let stocksTs = ["AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA", "AAA"];
+let stocksTs = ["AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA"];
 let pricesTs = [
-  [12.81, 11.09, 12.11, 10.93, 9.83, 8.14, 20],
-  [10.34, 10.56, 10.14, 12.17, 13.1, 11.22, 20],
-  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21, 20],
-  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21, 20],
+  [12.81, 11.09, 12.11, 10.93, 9.83, 8.14],
+  [10.34, 10.56, 10.14, 12.17, 13.1, 11.22],
+  [11.53, 10.67, 10.42, 11.88, 11.77, 10.21],
 ];
 
-console.log(stocksRankTs(stocksTs, pricesTs));
+console.log(stocksRank(stocksTs, pricesTs));
