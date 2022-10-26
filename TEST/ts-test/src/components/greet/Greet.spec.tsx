5d988aee13d75d4b33099b6c5e45c4c8081b1ac9
Alik64 <albert.barsamov@gmail.com>
2022-10-26 23:53:04 +0200

    use spec notation

--- TEST/ts-test/src/components/greet/Greet.spec.tsx
+++ TEST/ts-test/src/components/greet/Greet.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet renders corectly", () => {
+it("Greet renders corectly", () => {
   render(<Greet />);
   const textEl = screen.getByText(/hello/i);
   expect(textEl).toBeInTheDocument();
