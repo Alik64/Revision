0a271ad6cb0d19768b0d8708a8708c5cecea287e
Alik64 <albert.barsamov@gmail.com>
2022-10-29 22:57:41 +0200

    FCC : dropElements

--- FCC/Intermediate_Algorithm_Scripting/015_dropElement.js
+++ FCC/Intermediate_Algorithm_Scripting/015_dropElement.js
@@ -3,6 +3,13 @@
  * until the function func returns true when the iterated element is passed through it.
  *
  * Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+ * Waiting:dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+Waiting:dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+Waiting:dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
+Waiting:dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+Waiting:dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
+Waiting:dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
+
  */
 
 function dropElements(arr, func) {
@@ -15,5 +22,10 @@ function dropElements(arr, func) {
 console.log(
   dropElements([1, 2, 3], function (n) {
     return n < 3;
+  })
+);
+console.log(
+  dropElements([1, 2, 3, 7, 4], function (n) {
+    return n > 3;
   })
 );
