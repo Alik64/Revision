e2f1d6cf829e8a90bcb92808db65c0a352379a1c
Alik64 <albert.barsamov@gmail.com>
2022-11-02 10:54:18 +0100

    PreviewClock resolved

--- React/v18/src/index.js
+++ React/v18/src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
 // ReactDOM.render(
 
 //   <App />
