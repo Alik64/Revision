d43818971e9821dd6ad17666f0849390e5bc275c
Alik64 <albert.barsamov@gmail.com>
2022-11-09 21:53:20 +0100

    Nested folder structure

---
+++ Nextjs/codevolution/pages/blog/first.tsx
@@ -0,0 +1,5 @@
+function FirstBlog() {
+  return <h1>FirstBlog Page</h1>;
+}
+
+export default FirstBlog;
---
+++ Nextjs/codevolution/pages/blog/second.tsx
@@ -0,0 +1,5 @@
+function SecondBlog() {
+  return <h1>SecondBlog Page</h1>;
+}
+
+export default SecondBlog;
