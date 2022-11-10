c2c88dc5b16af07fa24fa95fd240f1a276985520
Alik64 <albert.barsamov@gmail.com>
2022-11-10 10:52:47 +0100

    catch all Routes

---
+++ Nextjs/codevolution/pages/docs/[[...params]].tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+
+function Doc() {
+  const router = useRouter();
+
+  const { params = [] } = router.query;
+  console.log(params);
+
+  if (params.length === 2) {
+    return (
+      <h1>
+        Vieving docs for <em>{params[0]}</em> and <em>{params[1]}</em>
+      </h1>
+    );
+  } else if (params.length === 1) {
+    return (
+      <h1>
+        Vieving docs for <em>{params[0]}</em>
+      </h1>
+    );
+  }
+  return <h1>Docs Homepage</h1>;
+}
+
+export default Doc;
