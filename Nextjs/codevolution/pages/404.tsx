f00473479d08f2cad7fbd3a274551feeb96da870
Alik64 <albert.barsamov@gmail.com>
2022-11-11 12:02:28 +0100

    custom 404

---
+++ Nextjs/codevolution/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  const center = {
+    display: "grid",
+    placeItems: "center",
+    height: "100vh",
+    fontSize: "72px",
+  };
+  return <div style={center}>404 🤷🏻‍♂️</div>;
+};
+
+export default NotFound;
