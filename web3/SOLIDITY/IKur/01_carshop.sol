cf906b8ba720f494a82ab0c41c6aa7f3b25672f5
Albert Barsamov <abarsamov@worldia.com>
2022-12-01 23:27:01 +0100

    Solidity/ IKur : add carShop.sol

---
+++ web3/SOLIDITY/IKur/01_carshop.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract CarShop {
+    mapping(address => bool) buyers;
+    uint256 public price = 2 ether;
+    address public owner;
+    address public shopAddress;
+
+    constructor() {
+        owner = msg.sender;
+        shopAddress = address(this);
+    }
+
+    function getBuyer(address _addr) public view returns (bool) {
+        require(owner == msg.sender, "You are not an owner!");
+
+        return buyers[_addr];
+    }
+
+    function addBuyer(address _addr) public {
+        require(owner == msg.sender, "You are not an owner!");
+        buyers[_addr] = true;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return shopAddress.balance;
+    }
+
+    receive() external payable {
+        //address: 0xf8e81D47203A594245E36C48e151709F0C19fBe8
+    }
+}
