4afb06f80005e9ce05e127daed5a4a54f80b1845
Albert Barsamov <abarsamov@worldia.com>
2022-12-25 22:44:10 +0100

    reduce exos

--- JavaScript/Basics/Arrays/reduce2.js
+++ JavaScript/Basics/Arrays/reduce2.js
@@ -10,3 +10,12 @@ const result = people.reduce((acc, obj) => {
 }, {});
 
 console.log(result);
+
+/*
+ {
+  '1': { data: { id: 1, name: 'Kyle', age: 26 } },
+  '2': { data: { id: 2, name: 'John', age: 31 } },
+  '3': { data: { id: 3, name: 'Sally', age: 42 } },
+  '4': { data: { id: 4, name: 'Jill', age: 42 } }
+}
+ */
