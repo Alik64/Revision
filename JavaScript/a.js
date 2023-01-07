0cc22475d469fded0fc74df256de705b64069376
Albert Barsamov <abarsamov@worldia.com>
2023-01-08 00:03:12 +0100

    update echauffement

--- JavaScript/a.js
+++ JavaScript/a.js
@@ -1,35 +1,17 @@
-const WIDTH = process.argv[2];
-const HEIGTH = process.argv[3];
-const heightLimit = HEIGTH - 1; // 2
-const widthLimit = WIDTH - 1; // 4
-const BAR = "-";
-const PILE = "|";
-const CORNER = "o";
-const SPACE = " ";
+const width = process.argv[2];
+const height = process.argv[3];
 
-console.log("width:", WIDTH, "height:", HEIGTH);
+// Vérifier que les arguments sont des nombres entiers positifs
+if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
+  console.log("Error: Invalid arguments");
+  process.exit(1);
+}
 
-for (let i = 0; i <= heightLimit; i++) {
-  let line = [];
-  for (let j = 0; j <= widthLimit; j++) {
-    if (
-      (i === 0 && j === 0) ||
-      (i === 0 && j === widthLimit) ||
-      (i === heightLimit && j === 0) ||
-      (i === heightLimit && j === widthLimit)
-    ) {
-      line.push(CORNER);
-      continue;
-    }
-    if (i > 0 && i < heightLimit) {
-      if (j === 0 || j === widthLimit) {
-        line.push(PILE);
-        continue;
-      }
-      line.push(SPACE);
-      continue;
-    }
-    line.push(BAR);
+// Afficher le rectangle
+for (let i = 0; i < height; i++) {
+  if (i === 0 || i === height - 1) {
+    console.log("o" + "-".repeat(width - 2) + "o");
+  } else {
+    console.log("|" + " ".repeat(width - 2) + "|");
   }
-  console.log(line.join(""));
 }
