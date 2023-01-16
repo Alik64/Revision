241dee689ee3abf2a93056579e1546b6c1e35394
Albert Barsamov <abarsamov@worldia.com>
2023-01-16 23:28:48 +0100

    job queue

--- NodeJS/AsynchonousJS/overview.js
+++ NodeJS/AsynchonousJS/overview.js
@@ -1,10 +1,12 @@
+// Callback Queue
 setTimeout(() => {
   console.log("1", "is the lonliest number");
 }, 0);
 setTimeout(() => {
   console.log("2", "can be as bad as one");
 }, 10);
-
+// Job Queue / Microtask
 Promise.resolve("hi").then((data) => console.log("2", data));
 
+// Synchro
 console.log("3", "is a crowd");
