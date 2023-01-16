218ad3f349702a05bd7b105ad9dd33185967009b
Albert Barsamov <abarsamov@worldia.com>
2023-01-16 23:15:04 +0100

    promiseAll

---
+++ NodeJS/AsynchonousJS/promiseAll.js
@@ -0,0 +1,11 @@
+const urls = [
+  "https://swapi.dev/api/people/1",
+  "https://swapi.dev/api/people/2",
+  "https://swapi.dev/api/people/3",
+];
+
+Promise.all(urls.map((url) => fetch(url).then((res) => res.json())))
+  .then((array) => {
+    console.table(array);
+  })
+  .catch((err) => console.log("error:", err));
