53fbda0caf0a02892d96d2ee7da82100cbc08b1a
Albert Barsamov <abarsamov@worldia.com>
2023-05-01 18:22:11 +0200

    remove product folder

--- Nextjs/codevolution/pages/blog/index.tsx
+++ Nextjs/codevolution/pages/blog/index.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Blog() {
-  return <h1>Blog Page</h1>;
+  return (
+    <>
+      <h1>Blog Page</h1>
+      <Link href="/">Home</Link>
+      <hr />
+      <Link href="/blog/first">First</Link>
+    </>
+  );
 }
 
 export default Blog;
