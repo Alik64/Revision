c8ac79a6859489b9ddeaa462fe9b782137e328c0
Albert Barsamov <abarsamov@worldia.com>
2023-08-13 23:08:04 +0200

    feat: using SPREAD in index.js

--- NodeJS/section4-modules/commonModules/internals/index.js
+++ NodeJS/section4-modules/commonModules/internals/index.js
@@ -1,6 +1,4 @@
-const request = require("./request.js");
-const response = require("./response.js");
 module.exports = {
-  send: request.send,
-  read: response.read,
+  ...require("./request.js"),
+  ...require("./response.js"),
 };
