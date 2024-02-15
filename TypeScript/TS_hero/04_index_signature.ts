f6d11ad14ec8814ee5761d250a80997687a42380
Albert Barsamov <abarsamov@worldia.com>
2024-02-15 17:36:32 +0100

    4: index signature: solution

---
+++ TypeScript/TS_hero/04_index_signature.ts
@@ -0,0 +1,15 @@
+
+type GroceryList = {
+    [produit:string]:number
+    }
+    
+    type InappropriateActionBySituation = {
+        [situation:string]: string[]
+    };
+    
+    type CharactersById = {
+        [num:number]:  {
+        [personID:string]: string | number
+      }
+    };
+    
