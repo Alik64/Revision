c0984679003d4a6c02d9b15ed7d4dd29e65c4f10
Albert Barsamov <abarsamov@worldia.com>
2024-02-16 23:26:56 +0100

    typeof operator: solution

--- TypeScript/TS_hero/04_index_signature.ts
+++ TypeScript/TS_hero/04_index_signature.ts
@@ -1,15 +1,13 @@
-
 type GroceryList = {
-    [produit:string]:number
-    }
-    
-    type InappropriateActionBySituation = {
-        [situation:string]: string[]
-    };
-    
-    type CharactersById = {
-        [num:number]:  {
-        [personID:string]: string | number
-      }
-    };
-    
+  [produit: string]: number;
+};
+
+type InappropriateActionBySituation = {
+  [situation: string]: string[];
+};
+
+type CharactersById = {
+  [num: number]: {
+    [personID: string]: string | number;
+  };
+};
--- TypeScript/TS_hero/05_typeof.ts
+++ TypeScript/TS_hero/05_typeof.ts
@@ -1,6 +1,75 @@
+//examples of typeof
+
+{
+  const name = "Euler's Number";
+  const value = 2.7182;
+
+  // we can use `typeof` for type aliases
+  type Value = typeof value;
+
+  interface FamousNumbers {
+    // or we can use it inline
+    label: typeof name;
+    value: Value;
+  }
+}
+
 type Width = typeof width;
 type Margin = unknown;
 type Data = unknown;
 type YScale = unknown;
 
 type D3ChartConfig = unknown;
+
+
+const height = 500;
+const width = 700;
+
+
+
+const margin = {
+  top: 20,
+  right: 30,
+  bottom: 40,
+  left: 50,
+};
+
+
+const d3ChartConfig = {
+  width,
+  height,
+  margin,
+  data: [
+    { category: 'A', value: 30 },
+    { category: 'B', value: 45 },
+    { category: 'C', value: 60 },
+    { category: 'D', value: 25 },
+    { category: 'E', value: 50 },
+  ],
+  xScale: {
+    type: 'band',
+    domain: [0, 100],
+    range: [0, width - margin.right - margin.left],
+  },
+  yScale: {
+    type: 'linear',
+    domain: [0, 100],
+    range: [height - margin.bottom, margin.top],
+  },
+  xAxis: {
+    label: 'Categories',
+    tickSize: 5,
+  },
+  yAxis: {
+    label: 'Values',
+    tickSize: 5,
+  },
+  bar: {
+    fill: 'rebeccapurple',
+  },
+};
+
+
+
+
+
