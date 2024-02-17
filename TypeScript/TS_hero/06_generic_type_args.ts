7ad7c59d2cbdfa0944e37b50ecb1cd3c7bc41f61
Albert Barsamov <abarsamov@worldia.com>
2024-02-17 11:08:24 +0100

    generic types constraints

--- TypeScript/TS_hero/06_generic_type_args.ts
+++ TypeScript/TS_hero/06_generic_type_args.ts
@@ -8,8 +8,8 @@
   }
 
   type NumberRow = Row<number>;
-  
-  type StringRow = Row<string>; //equal to 
+
+  type StringRow = Row<string>; //equal to
   {
     type StringRow = {
       label: string;
@@ -19,9 +19,9 @@
   }
 }
 
-type GroceryStore<Name,City> = {
-	name:Name;
-	city:City;
+type GroceryStore<Name, City> = {
+  name: Name;
+  city: City;
 };
 
 type GroceryItem<Name, Price, InStock> = {
@@ -30,5 +30,22 @@ type GroceryItem<Name, Price, InStock> = {
   inStock: InStock;
 };
 
-type CapreseSalad = GroceryItem<'Caprese Salad',14.99,true>;
+type CapreseSalad = GroceryItem<"Caprese Salad", 14.99, true>;
 
+{
+  // Generic Type Constraints
+  type RowConstraints = string | number | (() => string | number);
+  type Row<T extends RowConstraints> = {
+    value: T;
+    label: string;
+    orientation: "vertical" | "horizontal";
+  };
+
+  type AllowString<T extends string> = T;
+  type AllowNumber<T extends number> = T;
+
+  type CreateLogger<T extends (value: number) => void> = {
+    log: T;
+    exit: () => void;
+  };
+}
