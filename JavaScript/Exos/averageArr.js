335ddcfbfe6fa652f9757a84705d671944fed2a0
Albert Barsamov <abarsamov@worldia.com>
2024-02-24 00:38:03 +0100

    update averageArr

--- JavaScript/Exos/averageArr.js
+++ JavaScript/Exos/averageArr.js
@@ -1,6 +1,9 @@
-var arr = [1, 5, 2, 3, 7];
-
+let emptyArr = [];
+let arr = [1, 5, 2, 3, 7];
 function average(arr) {
+  if(arr.length === 0){
+    return 0
+  }
   let summ = 0;
   for (let i = 0; i < arr.length; i++) {
     summ += arr[i];
@@ -8,3 +11,4 @@ function average(arr) {
   return summ / arr.length;
 }
 console.log("average(arr) ==>", average(arr));
+console.log("average(empytArr) ==>", average(emptyArr));
