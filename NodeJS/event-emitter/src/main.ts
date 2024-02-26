def8f73bb17403c1d34ac1e4de1c1f50bbafa20c
Albert Barsamov <abarsamov@worldia.com>
2024-02-26 22:24:52 +0100

    chore: set up main.ts + index.html

--- NodeJS/event-emitter/src/main.ts
+++ NodeJS/event-emitter/src/main.ts
@@ -1,24 +1,8 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const chatContainerRef = document.querySelector('.chat-container') as HTMLDivElement;
+const messagesRef = document.querySelector('.messages') as HTMLDivElement;
+const loginFormRef = document.querySelector( '#login') as HTMLFormElement;
+const inputContainerRef = document.querySelector('.input-container') as HTMLFormElement;
+const signoutButtonRef = document. querySelector ('#sign-out') as HTMLButtonElement;
