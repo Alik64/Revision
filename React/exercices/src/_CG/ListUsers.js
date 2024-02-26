2e85b5e87b532b5b70082dd093b8dec920f635b2
Albert Barsamov <abarsamov@worldia.com>
2024-02-26 22:03:33 +0100

    upgrade storybook

--- React/exercices/src/_CG/ListUsers.js
+++ React/exercices/src/_CG/ListUsers.js
@@ -10,21 +10,20 @@ const ListUsers = ({ users }) => {
       <div className="user-count">
         Users: {typeof users !== "undefined" ? users.length : 0}
       </div>
-      {typeof users !== "undefined" && users.length > 0 ? (
+      {typeof users !== "undefined" && users.length > 0 && (
         <ul className="user-list">
           {users
             .sort((a, b) =>
               a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1
             )
-            .map((user) => {
-              return (
+            .map((user) =>  (
                 <li key={user.lastName} style={{ color: "white" }}>
                   {user.firstName} {user.lastName}
                 </li>
               );
-            })}
+            )}
         </ul>
-      ) : null}
+      )}
     </>
   );
 };
