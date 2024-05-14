b16e7979ab57a94bb725c405ca8787919e166784
Albert Barsamov <albert.barsamov@gmail.com>
2024-05-14 15:06:01 +0200

    feat: install rhf dev tool + handle submission

--- React/codevolution/src/components/YoutubeForm.tsx
+++ React/codevolution/src/components/YoutubeForm.tsx
@@ -1,18 +1,31 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
 export const YoutubeForm = () => {
+  const { register, control, handleSubmit } = useForm<FormValues>();
+  const onSubmit = (data: FormValues) => {
+    console.log("Form state:", data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" {...register("username")} id="username" />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" name="email" id="email" />
+        <input type="email" {...register("email")} id="email" />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+        <input type="text" {...register("channel")} id="channel" />
 
         <button type="submit">Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
