c231f84ef8359641019aa044454b3fdc1ead185c
Albert Barsamov <albert.barsamov@gmail.com>
2024-06-23 21:56:18 +0200

    feat: implement update func

--- Refine/src/App.tsx
+++ Refine/src/App.tsx
@@ -2,10 +2,12 @@ import { Refine } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
 import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
 function App() {
   return (
     <Refine dataProvider={dataProvider}>
-      <ShowProduct />
+      {/* <ShowProduct /> */}
+      <EditProduct />
     </Refine>
   );
 }
--- Refine/src/providers/data-provider.ts
+++ Refine/src/providers/data-provider.ts
@@ -4,30 +4,25 @@ const API_URL = "https://api.fake-rest.refine.dev";
 
 export const dataProvider: DataProvider = {
   getOne: async ({ resource, id, meta }) => {
-    try {
-      const response = await fetch(`${API_URL}/${resource}/${id}`);
-      if (response.status < 200 || response.status > 299) throw response;
-      const data = await response.json();
-      return { data };
-    } catch (error) {
-      throw new Error("Something went wrong with getOne");
-    }
+    const response = await fetch(`${API_URL}/${resource}/${id}`);
+    if (response.status < 200 || response.status > 299) throw response;
+    const data = await response.json();
+    return { data };
   },
   update: async ({ resource, id, variables }) => {
-    try {
-      const response = await fetch(`${API_URL}/${resource}/${id}`, {
-        method: "PATCH",
-        body: JSON.stringify(variables),
-        headers: {
-          "Content-type": "application/json;",
-        },
-      });
-      if (response.status < 200 || response.status > 299) throw response;
-      const data = await response.json();
-      return { data };
-    } catch (error) {
-      throw new Error("Something went wrong with update");
-    }
+    const response = await fetch(`${API_URL}/${resource}/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(variables),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status < 200 || response.status > 299) throw response;
+
+    const data = await response.json();
+
+    return { data };
   },
   getList: () => {
     throw new Error("Not implemented");
