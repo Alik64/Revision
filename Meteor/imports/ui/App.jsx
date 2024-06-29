e644b061e77b7b34531b6e4523ba6acbc20a784a
Albert Barsamov <albert.barsamov@gmail.com>
2024-06-29 15:26:11 +0200

    feat: introduction finished

--- Meteor/imports/ui/App.jsx
+++ Meteor/imports/ui/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
+import { Task } from "./Task.jsx";
+
+const tasks = [
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <ul>
+      {tasks.map((task) => (
+        <Task key={task._id} task={task} />
+      ))}
+    </ul>
   </div>
 );
