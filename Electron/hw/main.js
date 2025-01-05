b47e652f1b02067d7fa7f945d0824e4424e1c7d1
Albert Barsamov <albert.barsamov@gmail.com>
2025-01-05 22:23:12 +0100

    run hw app

---
+++ Electron/hw/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile('index.html'); // Charge le fichier HTML
+});
+
+// Quitter l'application lorsque toutes les fenêtres sont fermées
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
